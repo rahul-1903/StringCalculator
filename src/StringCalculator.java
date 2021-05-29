63677c77bbc3f84a5c20ae305dbb74a9cb38b246
Rahul Kumar <rahulkumargupta19@gmail.com>
2021-05-29 17:59:33 +0530

    refactored code to pass all tests

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,13 +1,14 @@
+import java.util.Arrays;
+
 public class StringCalculator {
     public static int Add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
-        } else if(numbers.contains(",")) {
-            String[] tokens = numbers.split(",");
-            return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
         }
-        else {
-            return Integer.parseInt(numbers);
+        String[] tokens = numbers.split(",");
+
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .sum();
         }
-    }
 }
