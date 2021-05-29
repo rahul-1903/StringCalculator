f43e7f1429f50d75a3a1e05071c69f667990a1e5
Rahul Kumar <rahulkumargupta19@gmail.com>
2021-05-29 17:49:07 +0530

    added new testcase which failed

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
+    @Test
+    public void addNumberInEmptyString() {
+        assertEquals(0, StringCalculator.Add(""));
+    }
 }
