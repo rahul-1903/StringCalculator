5adabf81b01fee1f08cde478067fb11cc5f395b9
Rahul Kumar <rahulkumargupta19@gmail.com>
2021-05-29 17:54:54 +0530

    changed code to pass all tests

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,6 +2,9 @@ public class StringCalculator {
     public static int Add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
+        } else if(numbers.contains(",")) {
+            String[] tokens = numbers.split(",");
+            return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
         }
         else {
             return Integer.parseInt(numbers);
